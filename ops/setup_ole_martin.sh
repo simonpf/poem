58e51ffab42ed9e0c435bc056d1eec8a69a1f487
Ole Martin Christensen <ole.martin.christensen@qamcom.se>
2019-07-02 18:10:50 +0200

    Fixed error in setup script.

--- ops/setup_ole_martin.sh
+++ ops/setup_ole_martin.sh
@@ -4,6 +4,6 @@ export PYTHONPATH=/home/olemar/MISU/MATS/invlib:${PYTHONPATH}
 source ~/intel/mkl/bin/mklvars.sh intel64
 
 # Force MKL preload to avoid undefined symbol problem.
-export LD_LIBRARY_PATH=/home/olemar/intel/compilers_and_libraries_2019.4.243/linux/mkl/lib/intel64_lin/libmkl_core.so:/home/olemar/intel/compilers_and_libraries_2019.4.243/linux/mkl/lib/intel64_lin/libmkl_intel_thread.so:/home/olemar/intel/compilers_and_libraries_2019.4.243/linux/compiler/lib/intel64/libiomp5.so
+export LD_PRELOAD=/home/olemar/intel/compilers_and_libraries_2019.4.243/linux/mkl/lib/intel64_lin/libmkl_core.so:/home/olemar/intel/compilers_and_libraries_2019.4.243/linux/mkl/lib/intel64_lin/libmkl_intel_thread.so:/home/olemar/intel/compilers_and_libraries_2019.4.243/linux/compiler/lib/intel64/libiomp5.so
 
 
